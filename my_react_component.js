e263863fa0acefc283aa988340b55b2bc4daafbe
denisflorjancic <florjancic.denis@gmail.com>
2020-03-19 08:30:54 +0100

    img wokring

--- my_react_component.js
+++ my_react_component.js
@@ -4,14 +4,17 @@ function formatName(user) {
 const user = {
   fisrtName: 'Denis',
   lastName: 'Florjancic',
-  // statusImg: 'img/gold_coin.jpg',
+  statusImg: 'img/gold_coin.jpg',
   isMember: true
 }
 
 // === JSX, syntax extension to JavaScript
 // === JSX, tags may contain children 
 // === JSX, everything is converted to a string before render, helps preventing XSS (cross-site-scripting) attacks
- 
+
+// === Adding Javascript expression in an attribute === 
+// === React DOM uses camelCase property naming, class > className, tabindex > tabIndex
+
 const element = (
   <div>
     <h1>
@@ -20,14 +23,9 @@ const element = (
     <h2>
       Staus: {user.isMember ? 'Golden account' : 'Free account'}
     </h2>
+    <img src={user.statusImg} width="100px"></img>
   </div>
 );
-
-// === Adding Javascript expression in an attribute === 
-// === React DOM uses camelCase property naming, class > className, tabindex > tabIndex
-// const img = (
-//   <img src={user.statusImg}></img>
-// );
 
 ReactDOM.render (
   element,
